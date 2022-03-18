c5a1b902661effb27b49eba0a5ca1a8dadc9a62f
Prabhat <prabhat@git.com>
2022-03-18 12:19:50 +0530

    my project

---
+++ src/test/java/com/firstproject/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.firstproject.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
